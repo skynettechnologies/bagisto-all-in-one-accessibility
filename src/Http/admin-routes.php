8879eaf238ed7be76698f968ae393f15d71665c6
iOS Developer <developer3@skynettechnologies.com>
2023-12-07 17:03:37 +0530

    Solve Migrations issues

--- src/Http/admin-routes.php
+++ src/Http/admin-routes.php
@@ -7,10 +7,11 @@ Route::group([
         'middleware'    => ['web', 'admin']
     ], function () {
 
-        Route::get('/allinoneaccessibility', 'SkynetTechnologies\AllinOneAccessibility\Http\Controllers\Admin\AllinOneAccessibilityController@index')->defaults('_config', [
+        Route::get('', 'SkynetTechnologies\AllinOneAccessibility\Http\Controllers\Admin\AllinOneAccessibilityController@index')->defaults('_config', [
             'view' => 'allinoneaccessibility::admin.index',
         ])->name('admin.allinoneaccessibility.index');
 
+        Route::get('/allinoneaccessibility', [AllinOneAccessibilityController::class, 'admin.allinoneaccessibility.index']);
         Route::get('/allinoneaccessibility', [AllinOneAccessibilityController::class, 'index']);
         Route::post('/store', [AllinOneAccessibilityController::class, 'store'])->name('admin.aioa.store');
         Route::post('/update/{id}', [AllinOneAccessibilityController::class, 'update'])->name('aioa.update');
